f4274f86ecfe72ea24f3c684a48a47034d87bf9e
SimonDanisch <sdanisch@gmail.com>
2021-04-12 12:51:21 +0200

    add labels

--- src/recipes.jl
+++ src/recipes.jl
@@ -2,7 +2,6 @@
 function AbstractPlotting.plot!(graph_plot::Combined{Any,S} where {S<:Tuple{<:AbstractGraph}})
     # create initial vertex positions
     graph = graph_plot[1]
-
     layout_node = map(graph) do graph
         A = adjacency_matrix(graph)
         return [Point2f0(p[1], p[2]) for p in NetworkLayout.Spring.layout(A)]
@@ -18,5 +17,16 @@ function AbstractPlotting.plot!(graph_plot::Combined{Any,S} where {S<:Tuple{<:Ab
     linesegments!(graph_plot, lines)
     # Plot vertices over lines
     reverse!(graph_plot.plots)
+
+    labels = get(graph_plot, :labels, Observable(nothing))
+    if !isnothing(labels[])
+        labels_pos = map(vertex_plot[1], labels) do positions, labels
+            if length(positions) != length(labels)
+                error("Need one label per vertex. Found $(length(labels)) labels and $(length(positions)) vertices.")
+            end
+            return map((a, b)-> (a, b), labels, positions)
+        end
+    end
+
     return graph_plot
 end
