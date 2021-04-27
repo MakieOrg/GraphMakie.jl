1b9195c0eca6eb184212468de4fa85d51cf268c8
Hans Würfel <git@wuerfel.io>
2021-04-27 14:42:48 +0200

    added some basic arguments to recipe

--- src/recipes.jl
+++ src/recipes.jl
@@ -1,32 +1,37 @@
+@recipe(GraphPlot, graph) do scene
+    Attributes(
+        layout = NetworkLayout.Spring.layout,
+        nodecolor = :red,
+        nodesize = 20,
+        marker = :circle,
+        edgecolor = :black,
+        edgewidth = 2.5,
+    )
+end
 
-function AbstractPlotting.plot!(graph_plot::Combined{Any,S} where {S<:Tuple{<:AbstractGraph}})
+function AbstractPlotting.plot!(gp::GraphPlot)
+    graph = gp[:graph]
+
     # create initial vertex positions
-    graph = graph_plot[1]
-    layout_node = map(graph) do graph
-        A = adjacency_matrix(graph)
-        return [Point2f0(p[1], p[2]) for p in NetworkLayout.Spring.layout(A)]
+    node_points = @lift begin
+        A = adjacency_matrix($graph)
+        ($(gp.layout))(A) # array of Point2f0
     end
+
+    edge_segments = @lift begin
+        indices = vec([getfield(e, s) for s in (:src, :dst), e in edges($graph)])
+        view($node_points, indices)
+    end
+
+    # plot edges
+    edge_plot = linesegments!(gp, edge_segments,
+                              color=gp.edgecolor,
+                              linewidth=gp.edgewidth)
 
     # plot vertices
-    vertex_plot = scatter!(graph_plot, layout_node)
-    # plot edges
-    lines = map(vertex_plot[1]) do x
-        indices = vec([getfield(e, src) for src in (:src, :dst), e in edges(graph[])])
-        return view(x, indices)
-    end
-    linesegments!(graph_plot, lines)
-    # Plot vertices over lines
-    reverse!(graph_plot.plots)
-
-    labels = get(graph_plot, :labels, Observable(nothing))
-    if !isnothing(labels[])
-        labels_pos = map(vertex_plot[1], labels) do positions, labels
-            if length(positions) != length(labels)
-                error("Need one label per vertex. Found $(length(labels)) labels and $(length(positions)) vertices.")
-            end
-            return map((a, b)-> (a, b), labels, positions)
-        end
-    end
-
-    return graph_plot
+    vertex_plot = scatter!(gp, node_points,
+                           color=gp.nodecolor,
+                           marker=gp.marker,
+                           markersize=gp.nodesize)
+    return gp
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,30 @@
+using GLMakie
 using GraphMakie
 using GraphMakie.LightGraphs
 using GraphMakie.AbstractPlotting
+using NetworkLayout
 using Test
 
 @testset "GraphMakie.jl" begin
-    g = wheel_digraph(10)
-    f, ax, p = plot(g)
-    f = plot_graph(g)
+    g = Node(wheel_digraph(10))
+    f, ax, p = graphplot(g)
+
+    # try to update graph
+    add_edge!(g[], 2, 4)
+    g[] = g[]
+    # try to update network
+    p.layout = NetworkLayout.SFDP.layout
+    # TODO: viewport stays constant uppon changing of g and layout, is this desired?
+
+    # update node observables
+    p.nodecolor = :blue
+    p.nodesize = 30
+    p.marker = :rect
+
+    # update edge observables
+    p.edgewidth = 5.0
+    p.edgecolor = :green
+
+    # it should be also possible to pass multiple values
+    f, ax, p = graphplot(g, nodecolor=[rand([:blue,:red,:green]) for i in 1:nv(g[])])
 end
